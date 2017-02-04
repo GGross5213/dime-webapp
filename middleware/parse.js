6ebeb40d0c0f683e8535117a520babcb80725a4b
GGross5213 <geoffreyrgross@gmail.com>
2017-02-04 12:41:11 -0600

    Temporarily add parse key

--- middleware/parse.js
+++ middleware/parse.js
@@ -1,5 +1,5 @@
 import Parse from 'parse/node'
-Parse.initialize(process.env.APP_ID);
+Parse.initialize(process.env.APP_ID || '11011011');
 Parse.serverURL = 'https://thawing-woodland-86198.herokuapp.com/parse'
 
 export default Parse
